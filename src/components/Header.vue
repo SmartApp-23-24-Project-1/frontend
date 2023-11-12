3a485bed8082e1614e4ff3018f5fec500f40efce
Stefano Mecocci <mecocci15@gmail.com>
2023-11-12 16:52:48 +0100

    added the user switch button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,8 @@
           <img src="/images/logo.png" alt="logo"/>
         </router-link>
       </div>
-      <div class="col-lg-10">
+
+      <div class="col-lg-8">
         <div class="navbar-menu-wrapper">
           <ul class="navbar-nav">
             <li class="nav-item">
@@ -15,6 +16,22 @@
                 Welcome, this is your performance summary today.
               </h1>
             </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-2 user-switch">
+        <div class="dropdown">
+          <button class="btn btn-primary dropdown-toggle user-switch__btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill user-switch__icon" viewBox="0 0 16 16">
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+            </svg> 
+            Maurizio
+          </button>
+
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">User 2</a></li>
+            <li><a class="dropdown-item" href="#">User 3</a></li>
           </ul>
         </div>
       </div>
@@ -30,5 +47,27 @@ export default {
 </script>
 
 <style scoped>
+.user-switch__icon {
+  fill: #eee;
+  width: 25px;
+  vertical-align: middle;
+  display: inline-block;
 
+  /* non mi veniva un modo migliore per allineare un svg */
+  transform: translate(-3px, -1.5px); 
+}
+
+.user-switch__btn {
+  background-color: #1F3BB3;
+  vertical-align: middle;
+  display: inline-block;
+}
+
+.user-switch__btn::after {
+  display: none;
+}
+
+.dropdown-menu {
+  min-width: 140px;
+}
 </style>
