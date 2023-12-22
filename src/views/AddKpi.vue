22e61c1d757e4f55917a30014ff24b5b894b319a
Stefano Mecocci <mecocci15@gmail.com>
2023-12-22 22:07:20 +0100

    fix AddKpi for "rawData undefined"

--- src/views/AddKpi.vue
+++ src/views/AddKpi.vue
@@ -13,16 +13,16 @@
             <div class="col-lg-8 form mt-3">
               <label for="kpi-name" class="form-label">Name</label>
               <input type="text" v-model="kpiname" class="form-control mb-4" id="kpi-name"
-                     placeholder="Put a name for your KPI">
+                placeholder="Put a name for your KPI">
               <label for="kpi-description" class="form-label">Description</label>
               <input type="text" v-model="kpidescription" class="form-control mb-4" id="kpi-description"
-                     placeholder="Put a short description for your KPI">
+                placeholder="Put a short description for your KPI">
               <label for="taxonomy" class="form-label">Taxonomy</label>
               <input type="text" v-model="taxonomy" class="form-control mb-4" id="taxonomy"
-                     placeholder="Put taxonomy for your KPI">
+                placeholder="Put taxonomy for your KPI">
               <label for="taxonomy" class="form-label">Range</label>
               <input type="text" v-model="range" class="form-control mb-4" id="taxonomy"
-                     placeholder="Describe the ranges in which your KPI value could be">
+                placeholder="Describe the ranges in which your KPI value could be">
               <label for="kpi-group" class="form-label">Group By</label>
               <select v-model="group_by" id="kpi-group" class="form-select">
                 <option :value="null" disabled selected>Select one</option>
@@ -51,15 +51,14 @@
                 <label for="kpi-formula" class="form-label">Formula</label>
                 <div class="col-lg-11">
                   <math-field id="inputFormula" class="w-100 form-control init" v-model="formula"
-                              @input="onInput"></math-field>
+                    @input="onInput"></math-field>
                 </div>
                 <div class="col-lg-1 px-0 my-auto">
                   <button class="icon-undo" @click.prevent="resetFormula">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 512 512">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                       <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                       <path
-                          d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z"/>
+                        d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z" />
                     </svg>
                   </button>
                 </div>
@@ -78,11 +77,11 @@
 
 <script>
 import axios from "axios";
-import {BASE_URL} from "@/constants/constants";
+import { BASE_URL } from "@/constants/constants";
 import router from "@/router";
 import Swal from "sweetalert2";
 import baseKeyboard from './baseKeyboard.json';
-import {MathfieldElement} from 'mathlive';
+import { MathfieldElement } from 'mathlive';
 
 export default {
   name: "AddKpi",
@@ -115,43 +114,48 @@ export default {
     this.$store.dispatch("getUnits");
 
     function chunk(array, size = 1) {
-        const length = array.length;
-        if (!length || size < 1) {
-            return [];
-        }
-        let index = 0;
-        let resIndex = 0;
-        const result = new Array(Math.ceil(length / size));
+      const length = array.length;
+      if (!length || size < 1) {
+        return [];
+      }
+      let index = 0;
+      let resIndex = 0;
+      const result = new Array(Math.ceil(length / size));
 
-        while (index < length) {
-            result[resIndex++] = array.slice(index, (index += size));
-        }
-        return result;
+      while (index < length) {
+        result[resIndex++] = array.slice(index, (index += size));
+      }
+      return result;
     }
 
     Promise.all([this.getKPIs(), this.getRawData()]).then(([kpis, rawData]) => {
-        const emptyRows = [["[separator]"], ["[separator]"], ["[separator]"]];
+      console.log(kpis, rawData);
 
-        let array = kpis.map(x => {
-          return {
-            label: x.name.replace("_", ""),
-            latex: x.name.replace("_", ""),
-            class: "small",
-            width: "1.5"
-          };
-        });
+      const emptyRows = [["[separator]"], ["[separator]"], ["[separator]"]];
 
-        let kpisKeyboard = [...(chunk(array, 4)), ["[separator]"], ["[separator]"]];
-        let rawDataKeyboard = [rawData.map(x => {
-          return {label: x.replace("_", ""), class: "small"};
-        }), ...emptyRows]; // il replace serve a evitare errori di escape  
+      let kpiButtons = kpis.map(x => {
+        return {
+          label: x.name.replace("_", ""),
+          latex: x.name.replace("_", ""),
+          class: "small",
+          width: "1.5"
+        };
+      });
 
-        let fullLayout = baseKeyboard["mathKeyboard"];
-        fullLayout[1].rows = kpisKeyboard;
-        fullLayout[2].rows = rawDataKeyboard;
+      let kpisKeyboard = [...(chunk(kpiButtons, 4)), ["[separator]"], ["[separator]"]];
 
-        mathVirtualKeyboard.layouts = fullLayout;
-      })
+      let rawDataKeyboard = [rawData.map(x => {
+        return { label: x.replace("_", ""), class: "small" };
+      }), ...emptyRows]; // il replace serve a evitare errori di escape  
+
+      let fullLayout = baseKeyboard["mathKeyboard"];
+      fullLayout[1].rows = kpisKeyboard;
+      fullLayout[2].rows = rawDataKeyboard;
+
+      mathVirtualKeyboard.layouts = fullLayout;
+
+      this.$store.commit("hideSpinner");
+    })
   },
   methods: {
     checkForm() {
@@ -175,39 +179,39 @@ export default {
     async addKPI() {
       this.kpis_formula = []; //tmp
       await axios.post(BASE_URL + "add_kpi", {
-            "name": this.kpiname,
-            "description": this.kpidescription,
-            "taxonomy": this.taxonomy,
-            "kpi_range": this.range,
-            "group_by": this.group_by,
-            "unit": this.unit,
-            "frequency": this.frequency,
-            "formula": this.formula,
-            "kpis": this.kpis_formula,
-            "raw_data": this.rd
-          },
-          {
-            headers: {
-              withCredentials: 'true',
-              'Authorization': 'Basic ' + btoa('smartapp' + ':' + 'api'),
-            }
-          }).then(() => {
-        Swal.fire({
-          title: "KPI added successfully",
-          icon: 'success',
-          confirmButtonText: 'Ok',
-          confirmButtonColor: '#1d41b2',
+        "name": this.kpiname,
+        "description": this.kpidescription,
+        "taxonomy": this.taxonomy,
+        "kpi_range": this.range,
+        "group_by": this.group_by,
+        "unit": this.unit,
+        "frequency": this.frequency,
+        "formula": this.formula,
+        "kpis": this.kpis_formula,
+        "raw_data": this.rd
+      },
+        {
+          headers: {
+            withCredentials: 'true',
+            'Authorization': 'Basic ' + btoa('smartapp' + ':' + 'api'),
+          }
+        }).then(() => {
+          Swal.fire({
+            title: "KPI added successfully",
+            icon: 'success',
+            confirmButtonText: 'Ok',
+            confirmButtonColor: '#1d41b2',
+          });
+          router.push('/library');
+        }).catch(() => {
+          Swal.fire({
+            title: "Something went wrong. Check data or try later.",
+            icon: 'error',
+            confirmButtonText: 'Ok',
+            confirmButtonColor: '#1d41b2',
+          });
+          router.push('/add-a-kpi');
         });
-        router.push('/library');
-      }).catch(() => {
-        Swal.fire({
-          title: "Something went wrong. Check data or try later.",
-          icon: 'error',
-          confirmButtonText: 'Ok',
-          confirmButtonColor: '#1d41b2',
-        });
-        router.push('/add-a-kpi');
-      });
     },
     async getRawData() {
       let response = await axios.get(BASE_URL + "raw_data", {
@@ -216,8 +220,8 @@ export default {
           'Authorization': 'Basic ' + btoa('smartapp' + ':' + 'api'),
         }
       });
-      this.rd = response.data["raw_data"];
-      return this.rd;
+      // this.rd = response.data.data.raw_data;
+      return response.data.data.raw_data;
     },
     async getKPIs() {
       let response = await axios.get(BASE_URL + "kpis", {
